60d1541f1c89e414781b47b6b463c2d592ba29f2
vonvonliu <49491354+vonvonliu@users.noreply.github.com>
2022-04-20 20:34:39 -0600

    Added choices for SelectField

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField, SelectField
 from wtforms.validators import DataRequired, length, Email, EqualTo, ValidationError
 
 from app.models import User
+
 
 class RegisterForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), length(min=3, max=20)])
@@ -22,28 +23,36 @@ class RegisterForm(FlaskForm):
         if user:
             raise ValidationError('Email already taken. Please choose another one')
 
-#login form from wtf
+
+# login form from wtf
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), length(min=3, max=20)])
     password = PasswordField('Password', validators=[DataRequired(), length(min=5, max=20)])
     remember = BooleanField('Remember')
     submit = SubmitField('Sign in')
 
-#manager page check the password
+
+# manager page check the password
 class ManagerForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired(), length(min=5, max=20)])
     submit = SubmitField('check')
 
-#ski form from wtf
+
+# ski form from wtf
 class SkiForm(FlaskForm):
-    ski_brand = StringField('ski_brand', validators=[DataRequired(), length(min=1, max=255)])
+    ski_brand = SelectField('ski_brand', choices=[('Nordia', 'Nordia'), ('Blizzard', 'Blizzard'),
+                                                  ('Atomic', 'Atomic'), ('Rossignol', 'Rossignol'),
+                                                  ('Elan', 'Elan'), ('Santa', 'Santa'), ('Black Crows', 'Black Crows')])
     ski_type = StringField('ski_type', validators=[DataRequired(), length(min=1, max=100)])
-    price = IntegerField('price', validators= [DataRequired()])
+    price = IntegerField('price', validators=[DataRequired()])
     submit = SubmitField('add in')
 
-#ski edit form from wtf
+
+# ski edit form from wtf
 class EditForm(FlaskForm):
-    ski_brand = StringField('ski_brand', validators=[DataRequired(), length(min=1, max=255)])
+    ski_brand = SelectField('ski_brand', choices=[('Nordia', 'Nordia'), ('Blizzard', 'Blizzard'),
+                                                  ('Atomic', 'Atomic'), ('Rossignol', 'Rossignol'),
+                                                  ('Elan', 'Elan'), ('Santa', 'Santa'), ('Black Crows', 'Black Crows')])
     ski_type = StringField('ski_type', validators=[DataRequired(), length(min=1, max=100)])
     price = IntegerField('price', validators=[DataRequired()])
     availability = StringField('availability', validators=[DataRequired(), length(min=1, max=255)])
