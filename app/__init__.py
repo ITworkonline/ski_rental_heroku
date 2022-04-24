9bcc091ab2488b235b6079880cf3c7a7f5fc7b6f
Sitong_Lu <493984502@qq.com>
2022-04-24 15:10:53 -0600

    retrack commit

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
-from config import Config
+from config import Config 
 #from flask_mysqldb import MySQL
 
 app = Flask(__name__)
