03c5d319459f8d34bc366ec9d49274f3c26381d1
ITworkonline <jiwa0171@colorado.edu>
2022-05-24 15:36:31 -0600

    Add some comments

--- app/routes.py
+++ app/routes.py
@@ -3,42 +3,54 @@ from flask_login import login_user, login_required, current_user, logout_user
 from datetime import datetime
 
 from app import app, bcrypt, db
-from app.forms import RegisterForm, LoginForm, ManagerForm, SkiForm, EditForm, PasswordResetRequestForm, ResetPasswordForm
+from app.forms import RegisterForm, LoginForm, ManagerForm, SkiForm, EditForm, PasswordResetRequestForm, \
+    ResetPasswordForm
 from app.models import User, Ski
 from app.email import send_reset_password_mail
 
-a = 1
-flag = True
+"""
+Global var:
+API_DATA: Pass the request data through diff func
+FLAG: Only pop up one data when POST 
+"""
+API_DATA = 1
+FLAG = True
 
+
+# root route
 @app.route('/')
 @login_required
 def index():
     return render_template('index.html')
 
+
+# API receiver
 @app.route('/key', methods=['GET', 'POST'])
 def weather_req():
-    global flag
-    global a
-    if flag == True:
-        flag = False
-        a = request.data
-        return a
-    return a
+    global FLAG
+    global API_DATA
+    if FLAG:
+        FLAG = False
+        API_DATA = request.data
+        return API_DATA
+    return API_DATA
 
 
+# about route
 @app.route('/about', methods=['GET', 'POST'])
 def about():
-    b = weather_req().decode("utf-8")
-    return render_template('about.html', temp=b)
+    context = weather_req().decode("utf-8")
+    return render_template('about.html', temp=context)
 
 
-
+# register route
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     # if the user is authenticated, direct to index page
     if current_user.is_authenticated:
         return redirect(url_for('index'))
     form = RegisterForm()
+    # store the username, email, and password in db
     if form.validate_on_submit():
         username = form.username.data
         email = form.email.data
@@ -51,8 +63,10 @@ def register():
     return render_template('register.html', form=form)
 
 
+# login route
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    # if the user is authenticated, direct to index page
     if current_user.is_authenticated:
         return redirect(url_for('index'))
     form = LoginForm()
@@ -75,7 +89,7 @@ def login():
     return render_template('login.html', form=form)
 
 
-# logout methods
+# logout route
 @app.route('/logout')
 @login_required
 def logout():
@@ -83,14 +97,14 @@ def logout():
     return redirect(url_for('login'))
 
 
-# base customer page
+# base customer route
 @app.route('/customer')
 @login_required
 def customer():
     return render_template('customer.html')
 
 
-# base manager page
+# base manager route
 @app.route('/manager', methods=['GET', 'POST'])
 @login_required
 def manager():
@@ -104,14 +118,14 @@ def manager():
     return render_template('manager.html', form=form)
 
 
-# manager sub page
+# manager sub route
 @app.route('/admin', methods=['GET', 'POST'])
 @login_required
 def admin():
     return render_template('admin.html')
 
 
-# manager dashboard
+# manager dashboard route
 @app.route('/dashboard', methods=['GET', 'POST'])
 @login_required
 def dashboard():
@@ -119,6 +133,7 @@ def dashboard():
     return render_template('dashboard.html', skis=ski)
 
 
+# add route
 @app.route('/add_ski', methods=['GET', 'POST'])
 @login_required
 def add_ski():
@@ -142,6 +157,7 @@ def insert(ski_brand, ski_type, price, availability):
     db.session.commit()
 
 
+# delete route
 @app.route('/delete_ski/<string:id>', methods=['POST'])
 @login_required
 def delete_ski(id):
@@ -152,12 +168,13 @@ def delete_ski(id):
     return redirect(url_for('dashboard'))
 
 
+# edit route
 @app.route('/edit_ski/<string:id>', methods=['GET', 'POST'])
 @login_required
 def edit_ski(id):
     edited = Ski.query.filter_by(id=id).first()
     form = EditForm(request.form)
-    # get the data
+    # get the data from db
     form.ski_brand.data = edited.ski_brand
     form.ski_type.data = edited.ski_type
     form.price.data = edited.price
@@ -183,34 +200,32 @@ def edit_ski(id):
     return render_template('edit_ski.html', form=form)
 
 
-# choose ski
+# choose route
 @app.route('/customer/<string:ski_brand>', methods=['GET', 'POST'])
 @login_required
 def choose_ski(ski_brand):
-    choosed = Ski.query.filter_by(ski_brand=ski_brand)
-    filtered_choosed = []
-    # check the db if certain brand is availalble or not
-    # print(choosed.first())
-    if choosed.first() is None:
+    choice = Ski.query.filter_by(ski_brand=ski_brand)
+    filtered_choice = []
+    # check the db if certain brand is available or not
+    if choice.first() is None:
         flash('No {} available right now, please choose other brands or contact with the manager'.format(ski_brand),
               category='danger')
         print('no brand case')
         return redirect(url_for('customer'))
 
-    for i in range(len(choosed.all())):
-        if choosed.all()[i].availability == 'Yes':
-            filtered_choosed.append(choosed.all()[i])
-    if filtered_choosed == []:
+    for i in range(len(choice.all())):
+        if choice.all()[i].availability == 'Yes':
+            filtered_choice.append(choice.all()[i])
+    if not filtered_choice:
         flash('No {} available right now, please choose other brands or contact with the manager'.format(
             ski_brand), category='danger')
         print('all booked case')
         return redirect(url_for('customer'))
 
-    # if filtered_choosed.first().availability == 'Yes':
-    return render_template('choose_ski.html', skis=filtered_choosed)
+    return render_template('choose_ski.html', skis=filtered_choice)
 
 
-# booking ski
+# booking route
 @app.route('/customer/pay/<string:id>', methods=['GET', 'POST'])
 @login_required
 def book_ski(id):
@@ -218,34 +233,35 @@ def book_ski(id):
     return render_template('book_ski.html', skis=booked)
 
 
-# successful booking
+# successful booking route
 @app.route('/successful/<string:id>', methods=['GET', 'POST'])
 @login_required
 def successful(id):
     availability = 'Booked by ' + current_user.username
     book_time = datetime.now()
     # after booking, update the certain ski be 'No' availability
-    successed = Ski.query.filter_by(id=id)
-    successed.first().availability = availability
-    successed.first().modification_time = book_time
+    ski = Ski.query.filter_by(id=id)
+    ski.first().availability = availability
+    ski.first().modification_time = book_time
     db.session.commit()
-    return render_template('successful.html', skis=successed)
+    return render_template('successful.html', skis=ski)
 
 
-# show the statistic for every ski brand
+# statistic route
 @app.route('/statistic', methods=['GET', 'POST'])
 @login_required
 def statistic():
     return render_template('statistic.html')
 
 
-# show the summary for all booked ski
+# statistic sub route
 @app.route('/statistic/<string:ski_brand>', methods=['GET', 'POST'])
 @login_required
 def statistic_car(ski_brand):
     brand_overview = Ski.query.filter_by(ski_brand=ski_brand)
     count = 0
     filtered_brand_overview = []
+    # count the price summary for the certain brand
     for i in range(len(brand_overview.all())):
         if brand_overview.all()[i].availability != 'Yes':
             count += brand_overview.all()[i].price
@@ -253,11 +269,14 @@ def statistic_car(ski_brand):
     return render_template('statistic_ski.html', skis=filtered_brand_overview, counts=count)
 
 
+# send password route
 @app.route('/send_password_reset_request', methods=['GET', 'POST'])
 def send_password_reset_request():
+    # if the user is authenticated, direct to index page
     if current_user.is_authenticated:
         return redirect(url_for('index'))
     form = PasswordResetRequestForm()
+    # get the email, user from db, send the mail
     if form.validate_on_submit():
         email = form.email.data
         user = User.query.filter_by(email=email).first()
@@ -267,11 +286,14 @@ def send_password_reset_request():
     return render_template('send_password_reset_request.html', form=form)
 
 
+# reset password route
 @app.route('/reset_password/<token>', methods=['GET', 'POST'])
 def reset_password(token):
+    # if the user is authenticated, direct to index page
     if current_user.is_authenticated:
         return redirect(url_for('index'))
     form = ResetPasswordForm()
+    # validate the token, change the password
     if form.validate_on_submit():
         user = User.check_reset_password_token(token)
         if user:
